ee5e69372587296ae825f3e41cf625a366999fe9
toho7 <tohohoya.7@gmail.com>
2026-01-04 17:20:35 +0900

    add debug page

--- routes/web.php
+++ routes/web.php
@@ -108,3 +108,13 @@ Route::middleware(['auth', 'child'])->group(function () {
     ->middleware('auth', 'child');
 });
 require __DIR__.'/auth.php';
+
+Route::get('/debug-env', function () {
+    return [
+        'DB_CONNECTION' => env('DB_CONNECTION'),
+        'DB_HOST' => env('DB_HOST'),
+        'DB_DATABASE' => env('DB_DATABASE'),
+        'DB_USERNAME' => env('DB_USERNAME'),
+        'DB_PASSWORD' => env('DB_PASSWORD'),
+    ];
+});
