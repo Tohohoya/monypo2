ce269208fe6a862b8a150dcef559d3b05a5e660f
toho7 <tohohoya.7@gmail.com>
2026-01-04 16:55:30 +0900

    Fix autoload path for Railway

--- public/index.php
+++ public/index.php
@@ -11,10 +11,10 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 }
 
 // Register the Composer autoloader...
-require '/virtual/smuw8kqa21erf/monypo/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once '/virtual/smuw8kqa21erf/monypo/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
